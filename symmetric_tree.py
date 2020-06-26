cb775323ee0d78ff82c1d220f393993f2e58f36a
Jorge <jorgeandre93@yahoo.com.br>
2020-06-26 15:58:20 +0800

    Update symmetric_tree.py

--- symmetric_tree.py
+++ symmetric_tree.py
@@ -9,8 +9,17 @@ class Solution:
         if not root: return True
         
         return self.recursion(root.left, root.right)
-        
+    
+    
     def recursion(self, node1, node2):
+        if not node1 or not node2: 
+            return node1 == node2
+        if node1.val != node2.val:
+            return False
+        return self.recursion(node1.left, node2.right) and self.recursion(node1.right, node2.left)
+    
+    
+    def recursion2(self, node1, node2):
         if not node1 and  not node2: 
             return True
         elif not node1 or not node2:
